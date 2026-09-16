cbf80835f4da2c298265b3bfe3b22eaa9f131264
Feather <feaher@hotmail.com>
2026-09-16 13:21:03 +0800

    fix router bug

---
+++ assets/Home-DE-xNErB.js
@@ -0,0 +1,1 @@
+import{t as e}from"./Privacy-DwIeKsio.js";import{f as t,g as n,p as r,t as i,u as a}from"./index-As8KgEVU.js";var o={class:`min-h-screen overflow-x-clip text-black dark:text-white`},s=r({__name:`Home`,setup(r){let{t:s,tm:c,rt:l}=i();return(r,i)=>(n(),a(`main`,o,[t(e)]))}});export{s as default};
